c854579fc72639339bdd7d9ee841dcfa1ea20194
marsian8 <spanarve@gmail.com>
2023-01-29 17:43:48 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main>
-      
+      <h1 className='text-6xl font-black text-green-900'>Hi Riya</h1>
     </main>
   )
 }
