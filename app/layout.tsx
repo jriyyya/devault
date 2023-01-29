c80982cee0dc75e806012d4a47cf141f0c4fd002
jriyyya <jriya.shi@gmail.com>
2023-01-29 17:18:10 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Montserrat } from '@next/font/google'
+
+const montserrat = Montserrat({variable:'--font-montserrat'})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className={`${montserrat.className}`}>{children}</body>
+    </html>
+  )
+}
